6c2285634a80c0781fd47fc9837976601099a6fd
James Hampton <jphampt@gmail.com>
2018-09-09 15:18:22 -0700

    Connected android app to google API

--- app/src/main/java/com/jphampton/hearthstonecardbacks/service/HearthstoneServiceImpl.java
+++ app/src/main/java/com/jphampton/hearthstonecardbacks/service/HearthstoneServiceImpl.java
@@ -3,6 +3,9 @@ package com.jphampton.hearthstonecardbacks.service;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.endpoints.cardback.cardback.Cardback;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.common.base.Optional;
 import com.jphampton.hearthstonecardbacks.models.Card;
 
@@ -30,32 +33,36 @@ public class HearthstoneServiceImpl implements HearthstoneService {
     @Override
     public Disposable getRankedCards(Consumer<List<Card>> onCompletion) {
         return Single.<List<Card>>create(e -> {
-                List<Card> cardbacks = getCardList();
-                e.onSuccess(cardbacks);
-            })
-            .cache()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(onCompletion);
+            List<Card> cardbacks = getCardList();
+            e.onSuccess(cardbacks);
+        })
+                .cache()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(onCompletion);
     }
 
     @Override
     public Disposable getCardByDate(Month month, int year, Consumer<Optional<Card>> onCompletion) {
         Log.e("waterlily", "getting card by date");
         return Single.<Optional<Card>>create(e -> {
-                @Nullable Card cardback = getCardbackFromServer(month, year);
-                e.onSuccess(Optional.fromNullable(cardback));
-            })
-            .cache()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(onCompletion);
+            @Nullable Card cardback = getCardbackFromServer(month, year);
+            e.onSuccess(Optional.fromNullable(cardback));
+        })
+                .cache()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(onCompletion);
     }
 
     @Nullable
-    private Card getCardbackFromServer(Month month, int year) {
+    private Card getCardbackFromServer(Month month, int year) throws IOException {
         // TODO: call our server endpoint
-        return new Card.Builder().build();
+        Cardback.Builder builder = new Cardback.Builder(
+                AndroidHttp.newCompatibleTransport(), new GsonFactory(), null);
+        Cardback service = builder.build();
+        com.endpoints.cardback.cardback.model.Cardback curr = service.currentCardback(month.toString(), year).setKey(PrivateConstants.API_KEY).execute();
+        return cardbackToCard(curr);
     }
 
     private String getCardString() {
@@ -64,7 +71,7 @@ public class HearthstoneServiceImpl implements HearthstoneService {
             URL url = new URL("https://omgvamp-hearthstone-v1.p.mashape.com/cardbacks");
             URLConnection urlConnection = setMashapeToken(url);
             BufferedReader reader = new BufferedReader(
-                new InputStreamReader(urlConnection.getInputStream()));
+                    new InputStreamReader(urlConnection.getInputStream()));
             String line;
             while ((line = reader.readLine()) != null) {
                 sb.append(line);
@@ -89,25 +96,40 @@ public class HearthstoneServiceImpl implements HearthstoneService {
                 cardList.add(jsonToCard(currJSON));
             }
         } catch (JSONException e) {
-           Log.w("JSON", "Failed to parse JSON.\n" + e);
+            Log.w("JSON", "Failed to parse JSON.\n" + e);
         }
         return cardList;
     }
 
     private Card jsonToCard(JSONObject json) throws JSONException {
         return new Card.Builder()
-            .setId(json.getInt("id"))
-            .setName(json.getString("name"))
-            .setDescription(json.getString("description"))
-            .setSource(json.getString("source"))
-            .setSourceDescription(json.getString("sourceDescription"))
-            .setEnabled(json.getBoolean("enabled"))
-            .setImgURL(json.getString("imgURL"))
-            .setImgAnimatedURL(json.getString("imgAnimatedURL"))
-            .setSortCategory(json.getInt("sortCategory"))
-            .setSortOrder(json.getInt("sortOrder"))
-            .setLocale(json.getString("locale"))
-            .build();
+                .setId(json.getInt("id"))
+                .setName(json.getString("name"))
+                .setDescription(json.getString("description"))
+                .setSource(json.getString("source"))
+                .setSourceDescription(json.getString("sourceDescription"))
+                .setEnabled(json.getBoolean("enabled"))
+                .setImgURL(json.getString("imgURL"))
+                .setImgAnimatedURL(json.getString("imgAnimatedURL"))
+                .setSortCategory(json.getInt("sortCategory"))
+                .setSortOrder(json.getInt("sortOrder"))
+                .setLocale(json.getString("locale"))
+                .build();
+    }
+
+    private Card cardbackToCard(com.endpoints.cardback.cardback.model.Cardback cardback) {
+        return new Card.Builder()
+                .setImgURL(cardback.getImgURL())
+                .setId(cardback.getId())
+                .setName(cardback.getMashapeName())
+                .setDescription("")
+                .setEnabled(true)
+                .setImgAnimatedURL("")
+                .setLocale("")
+                .setSortCategory(0)
+                .setSortOrder(0)
+                .setSource("")
+                .setSourceDescription("").build();
     }
 
     private URLConnection setMashapeToken(URL url) throws IOException {
